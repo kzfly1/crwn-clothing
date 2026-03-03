fa120069b0ed2ab8985fd5413ec070a2d0a57692
shawn <shawngidev@gmail.com>
2026-03-03 18:02:03 +1100

    set up react router and basic navigation bar

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,23 @@
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <div>Logo</div>
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+        </div>
+        <h1>I am the navigation bar</h1>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
